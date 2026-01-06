c48b07eeb22488cd67e172bcbfd93c5d591526cb
Duncan Lin <duncanlin.dev@gmail.com>
2026-01-06 19:14:28 +0800

    feat: 完成 724 使用 prefix sum 然後 基於 補數的概念來計算中間值

--- 724/724.go
+++ 724/724.go
@@ -2,8 +2,27 @@ package main
 
 func main() {
 
+	pivotIndex([]int{1, 7, 3, 6, 5, 6})
 }
 
+// Prefix Sum (Optimized Space)
+// Time Complexity: O(N)
+// Space Complexity: O(1)
 func pivotIndex(nums []int) int {
-	return 1
+	totalSum := 0
+	for _, num := range nums {
+		totalSum += num
+	}
+
+	leftSum := 0
+	for i, num := range nums {
+		// Right Sum = Total Sum - Left Sum - nums[i]
+		// We want check: Left Sum == Right Sum
+		if leftSum == totalSum-leftSum-num {
+			return i
+		}
+		leftSum += num
+	}
+
+	return -1
 }
