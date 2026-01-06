2a1e0cf938fe7035837f487e84e3d53b7057846c
Duncan Lin <duncanlin.dev@gmail.com>
2026-01-06 13:26:58 +0800

    feat: 完成 1143 的  2d DP 邏輯

---
+++ 1143/1143.go
@@ -0,0 +1,43 @@
+package main
+
+func main() {
+
+	// 	Given two strings text1 and text2, return the length of their longest common subsequence. If there is no common subsequence, return 0.
+
+	// A subsequence of a string is a new string generated from the original string with some characters (can be none) deleted without changing the relative order of the remaining characters.
+
+	// For example, "ace" is a subsequence of "abcde".
+	// A common subsequence of two strings is a subsequence that is common to both strings.
+	longestCommonSubsequence("abcde", "ace")
+
+	// excepted 2
+	longestCommonSubsequence("ezupkr", "ubmrapg")
+}
+
+func longestCommonSubsequence(text1 string, text2 string) int {
+
+	dp := make([][]int, len(text1)+1)
+	for i := range dp {
+		dp[i] = make([]int, len(text2)+1)
+	}
+
+	for i := 1; i <= len(text1); i++ {
+		for j := 1; j <= len(text2); j++ {
+			if text1[i-1] == text2[j-1] {
+				dp[i][j] = dp[i-1][j-1] + 1 // 找到相同，長度+1
+			} else {
+				dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+			}
+
+		}
+	}
+	return dp[len(text1)][len(text2)]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
