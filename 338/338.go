dbc1b71df9b2d1111e89d2d3f668a613ea786cbd
Duncan Lin <duncanlin.dev@gmail.com>
2026-01-08 03:48:12 +0800

    feat: 新增 338註解

--- 338/338.go
+++ 338/338.go
@@ -1,5 +1,7 @@
 package main
 
+// Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
+
 func main() {
 	n := 2
 	countBits(n)
